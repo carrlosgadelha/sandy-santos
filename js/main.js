d532d6f31293499627c66acc3ed591de63ed66ce
Carlos Gadelha <antoniocarlosgadelha70@gmail.com>
2024-07-23 13:54:34 -0300

    Finalizado a sessão de fotos e adicionado sessão de perguntas frequentes.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!")
+(function(){
+    const questionBtn = document.querySelector("#question-btn")
+    const answer = document.querySelector(".answer")
+
+    questionBtn.addEventListener("click", (e) => {
+        answer.classList.toggle("show-answer")
+    })
+})()
