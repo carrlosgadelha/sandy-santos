2614e0a4a576e1159c166dce9c88aaad2225c750
Carlos Gadelha <antoniocarlosgadelha70@gmail.com>
2024-07-26 21:55:08 -0300

    Finalizado a sessão de perguntas frequentes, inicio da sessão de contato e finalizado rodapé

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 (function(){
-    const questionBtn = document.querySelector("#question-btn")
-    const answer = document.querySelector(".answer")
-
-    questionBtn.addEventListener("click", (e) => {
-        answer.classList.toggle("show-answer")
+    const questions = document.getElementsByClassName("question-card")
+    Array.from(questions).forEach((el) => {
+        let answerMargin
+        el.addEventListener("click", () => {
+            const answer = el.nextElementSibling
+            if(answer.style.marginTop != "0px"){
+                answerMargin = answer.style.marginTop
+                answer.style.marginTop = "0px"
+            } else {
+                answer.style.marginTop = answerMargin
+            }
+        })
     })
 })()
