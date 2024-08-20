a5aa0e4bb1a56268ab5b1679565a8b20d2514036
Carlos Gadelha <antoniocarlosgadelha70@gmail.com>
2024-08-19 22:20:38 -0300

    Melhorado o sistema de agendamento.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,26 @@
     const agendarBtn = document.getElementById("agendarBtn")
     const modal = document.querySelector("#agendarPopup")
 
+    const dayOfModal = document.getElementById("day-of")
+    const d = new Date(2024, 11,1)
+    const lastDay = new Date(d.getFullYear(), d.getMonth() + 1, 0)
+
+    const days = []
+
+    for(let i = 0; i <= (lastDay.getDate() - d.getDate()); i++){
+        days.push(d.getDate() + i)
+    }
+
+    console.log(lastDay)
+
+    days.forEach(function(el){
+        const newDate = document.createElement("option")
+        newDate.value = `day-object:${el}`
+        newDate.innerHTML = `${el.toString().length == 1 ? `0${el}` : el}/${(d.getMonth() +1).toString().length <= 1 ? `0${(d.getMonth() +1)}` : d.getMonth() +1}/${d.getFullYear()}`
+
+        dayOfModal.appendChild(newDate)
+    })
+
     Array.from(questions).forEach((el) => {
         let answerMargin
         el.addEventListener("click", () => {
